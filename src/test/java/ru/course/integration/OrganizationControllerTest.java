51812c38f2f735eae76c88e92263efb1edfa0069
MaximFalaleev <maximfalaleev@mail.ru>
2018-07-28 18:25:39 +0400

    Switch to Spring Boot

--- src/test/java/ru/course/integration/OrganizationControllerTest.java
+++ src/test/java/ru/course/integration/OrganizationControllerTest.java
@@ -1,13 +1,31 @@
 package ru.course.integration;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import ru.course.Application;
+import ru.course.dto.ResponseDto;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
-@WebAppConfiguration
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = Application.class)
 public class OrganizationControllerTest {
+    @LocalServerPort
+    private String port;
 
+    private TestRestTemplate restTemplate = new TestRestTemplate();
+
+    @Test
+    public void testGet(){
+        ResponseEntity<ResponseDto> response = restTemplate.getForEntity("http://localhost:"+port+"/api/organization/2", ResponseDto.class);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
 }
